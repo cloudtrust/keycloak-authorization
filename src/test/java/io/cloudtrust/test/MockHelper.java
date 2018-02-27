e824aeb1a3c5fdf5fdf6851869f5d3ee986dd578
Alistair Doswald <alistair.doswald@elca.ch>
2018-02-27 18:03:49 +0100

    Working version of keycloak-authorization
    All additions necessary to make the module run have been done. This
    commit adds the files necessary to make the module run under keycloak.
    
    Still TODO is the documentation and a modification to the GUI that
    allows to differentiate the local authorization from the remote
    authrorization for OIDC.

--- src/test/java/io/cloudtrust/test/MockHelper.java
+++ src/test/java/io/cloudtrust/test/MockHelper.java
@@ -40,7 +40,6 @@ import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
